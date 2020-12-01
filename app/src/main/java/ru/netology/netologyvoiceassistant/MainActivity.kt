542cc7c0a64facf63ba3682c892fa603bf51a215
Alexey Verein <tpom6oh@gmail.com>
2020-12-01 20:47:00 +0300

    Types and text output

--- app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
+++ app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
@@ -2,10 +2,17 @@ package ru.netology.netologyvoiceassistant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myText: String = "Hello, world!"
+        val myNumber: Int = 42
+        val myFloatingNumber: Double = 3.14
+
+        findViewById<TextView>(R.id.text_output).text = myText + " " + myNumber + " " + myFloatingNumber
     }
 }
