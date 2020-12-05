97108fd9d8b7e12a055b6fd4c28341788c7b19fd
Alexey Verein <tpom6oh@gmail.com>
2020-12-05 18:54:06 +0300

    Add material top bar and material text input field

--- app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
+++ app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
@@ -3,7 +3,6 @@ package ru.netology.netologyvoiceassistant
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,14 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myText = "Hello, world!"
-        val myNumber = 42
-        val myFloatingNumber = 3.14
-
-        val outputText = "$myText $myNumber $myFloatingNumber"
-
-        val textView = findViewById<TextView>(R.id.text_output)
-        textView.text = outputText
+        setSupportActionBar(findViewById(R.id.topAppBar))
 
         Log.d("netology voice", "end of onCreate function")
     }
