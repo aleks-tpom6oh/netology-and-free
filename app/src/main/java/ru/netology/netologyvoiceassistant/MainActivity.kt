a0bf401e8de5cda839c7aec417c8cb41f1c1eb7b
Alexey Verein <tpom6oh@gmail.com>
2020-12-05 18:10:47 +0300

    Use string template and add logs

--- app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
+++ app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
@@ -2,17 +2,24 @@ package ru.netology.netologyvoiceassistant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("netology voice", "start of onCreate function")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myText: String = "Hello, world!"
-        val myNumber: Int = 42
-        val myFloatingNumber: Double = 3.14
+        val myText = "Hello, world!"
+        val myNumber = 42
+        val myFloatingNumber = 3.14
 
-        findViewById<TextView>(R.id.text_output).text = myText + " " + myNumber + " " + myFloatingNumber
+        val outputText = "$myText $myNumber $myFloatingNumber"
+
+        val textView = findViewById<TextView>(R.id.text_output)
+        textView.text = outputText
+
+        Log.d("netology voice", "end of onCreate function")
     }
 }
