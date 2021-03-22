c870dc8b1dc605d8656270c1fc6e052883c0e2af
Nikita Shumskii <lordsxrom@gmail.com>
2021-03-22 22:38:43 +0300

    HW2. Revision v2

--- app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
+++ app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
@@ -1,8 +1,10 @@
 package ru.netology.netologyvoiceassistant
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +15,24 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.topAppBar))
 
         Log.d("netology voice", "end of onCreate function")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_search -> {
+                // TODO: Add Wolfram request
+                return true
+            }
+            R.id.action_voice -> {
+                // TODO: Add Voice recognition
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
