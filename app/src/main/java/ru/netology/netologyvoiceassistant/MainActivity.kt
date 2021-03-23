6facbf020600656ee8865a983538363669a460a7
Nikita Shumskii <lordsxrom@gmail.com>
2021-03-23 18:35:22 +0300

    HW4. Revision v2

--- app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
+++ app/src/main/java/ru/netology/netologyvoiceassistant/MainActivity.kt
@@ -1,20 +1,29 @@
 package ru.netology.netologyvoiceassistant
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.speech.tts.TextToSpeech
+import android.speech.tts.UtteranceProgressListener
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.ListView
 import android.widget.SimpleAdapter
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.wolfram.alpha.WAEngine
 import com.wolfram.alpha.WAPlainText
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.util.*
+import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,6 +35,12 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var waEngine: WAEngine
 
+    lateinit var stopButton: FloatingActionButton
+
+    lateinit var textToSpeech: TextToSpeech
+
+    val TTS_REQUEST_CODE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d("netology voice", "start of onCreate function")
         super.onCreate(savedInstanceState)
@@ -34,6 +49,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.topAppBar))
         initViews()
         initWolframEngine()
+        initTts()
 
         Log.d("netology voice", "end of onCreate function")
     }
@@ -51,7 +67,18 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
             R.id.action_voice -> {
-                // TODO: Add Voice recognition
+                val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+                intent.putExtra(
+                    RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+                )
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "What do you want to know?")
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.US)
+                try {
+                    startActivityForResult(intent, TTS_REQUEST_CODE)
+                } catch (a: ActivityNotFoundException) {
+                    Toast.makeText(applicationContext, "TTS not found", Toast.LENGTH_SHORT).show()
+                }
                 return true
             }
             else -> return super.onOptionsItemSelected(item)
@@ -70,6 +97,17 @@ class MainActivity : AppCompatActivity() {
             intArrayOf(R.id.request, R.id.response)
         )
         searchesList.adapter = searchesAdapter
+        searchesList.setOnItemClickListener { _, _, position, _ ->
+            val request = searches[position]["Request"]
+            val response = searches[position]["Response"]
+            textToSpeech.speak(response, TextToSpeech.QUEUE_FLUSH, null, request)
+        }
+
+        stopButton = findViewById<FloatingActionButton>(R.id.stop_button)
+        stopButton.setOnClickListener {
+            textToSpeech.stop()
+            stopButton.visibility = View.GONE
+        }
     }
 
     fun initWolframEngine() {
@@ -108,6 +146,33 @@ class MainActivity : AppCompatActivity() {
                     put("Response", response)
                 })
                 searchesAdapter.notifyDataSetChanged()
+                textToSpeech.speak(response, TextToSpeech.QUEUE_FLUSH, null, request)
+            }
+        }
+    }
+
+    fun initTts() {
+        textToSpeech = TextToSpeech(this) {}
+        textToSpeech.language = Locale.US
+        textToSpeech.setOnUtteranceProgressListener(object : UtteranceProgressListener() {
+            override fun onStart(utteranceId: String?) {
+                stopButton.post { stopButton.visibility = View.VISIBLE }
+            }
+
+            override fun onDone(utteranceId: String?) {
+                stopButton.post { stopButton.visibility = View.GONE }
+            }
+
+            override fun onError(utteranceId: String?) {}
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == TTS_REQUEST_CODE && resultCode == RESULT_OK) {
+            data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)?.get(0)?.let { question ->
+                requestInput.text = question
+                askWolfram(question)
             }
         }
     }
